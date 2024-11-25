553510f17328c39aadad85c320170e5d3101e709
Nisse_S <100091408+Fikiuq@users.noreply.github.com>
2024-11-25 14:28:11 +0100

    Create tetris.js

---
+++ tetris.js
@@ -0,0 +1,5 @@
+// Een eenvoudige Tetris-game in JavaScript (gebruik een bibliotheek of werk verder)
+var canvas = document.getElementById('tetris');
+var ctx = canvas.getContext('2d');
+
+// Voeg hier de logica voor Tetris toe (gebruik een bestaande implementatie of bouw zelf een)
